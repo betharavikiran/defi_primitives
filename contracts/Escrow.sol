a6d1388a430cb48e6842e3332d2ceb790fa49606
betharavikiran <betha.ravikiran@gmail.com>
2024-04-20 11:05:25 +0530

    updated

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -3,14 +3,13 @@ pragma solidity ^0.8.14;
 
 import { ERC20 } from "contracts/ERC20.sol";
 
-
 contract Escrow {
     // Define state variables
-    ERC20 public token;
-    address public buyer;
-    address public seller;
-    uint256 public value;
-    uint256 public deadline;
+    ERC20 public token; // Payment against delivery of goods
+    address public buyer; // Buyer of the Asset
+    address public seller; // Seller of the Asset
+    uint256 public value;  // Value of gooding being purchased
+    uint256 public deadline; // Time line with in which the transaction should be completed.
 
     // Event to signal the completion of a trade
     event TradeCompleted(
@@ -27,16 +26,18 @@ contract Escrow {
     }
 
     // Function to deposit funds into the escrow
-    function deposit() public payable {
-        require(msg.value == value, "Incorrect deposit amount");
+    // The asset value and the Native token value is considered as equal for simplification
+    function deposit(uint value_) public {
         require(buyer == address(0), "Deposit already made");
+        // tokens are pulled from Buyer into this contract
+        token.transferFrom(buyer, address(this), value_);
         buyer = msg.sender;
     }
 
     // Function for the seller to confirm that the goods have been delivered
-    function confirmDelivery() public {
+    function acceptTransaction() public {
         require(seller == address(0), "Delivery already confirmed");
-        require(msg.sender == buyer, "Only the buyer can confirm delivery");
+        require(block.timestamp <= deadline, "Deadline has passed");
         seller = msg.sender;
     }
 
@@ -55,7 +56,7 @@ contract Escrow {
         require(buyer != address(0), "No deposit made");
         require(seller == address(0), "Delivery not confirmed");
         require(block.timestamp >= deadline, "Deadline has not passed");
-        require(msg.sender == seller, "Only the seller can initiate a refund");
+        require(msg.sender == buyer, "Only the Buyer can initiate a refund");
         payable (buyer).transfer(value);
     }
 }
